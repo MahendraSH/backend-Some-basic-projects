9835e61b2095e32bb4221065d076be2648d0f236
MahendrSH <maahi2001sh@gmail.com>
2023-04-08 07:25:05 +0530

    working correct

--- weartherApi/app.js
+++ weartherApi/app.js
@@ -1,48 +1,48 @@
 const { json } = require("express");
 const express = require("express");
-const bodyParser=require('body-parser')
-const https =require("node:https");
-const dotenv= require('dotenv').config();
+const bodyParser = require('body-parser')
+const https = require("node:https");
+const dotenv = require('dotenv').config();
 
-const app =express();
-app.use(bodyParser.urlencoded({extended:true}));
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/',(req,res)=>{
-    res.sendFile(__dirname+'/index.html')
-    
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+
 })
-app.post("/",(req,res)=>{
+app.post("/", (req, res) => {
 
-    const query=req.body.CityName;
-    
-const url =process.env.url
+    const query = req.body.CityName;
+
+    const key = process.env.key;
+    const url = process.env.url + query + key;
 
 
-https.get(url,(respos)=>
-    {
+    https.get(url, (respos) => {
         console.log(respos.statusCode);
-        respos.on("data",(data)=>{
-                 const weatherData=JSON.parse(data);
-                //  console.log(weatherData);
-                const temp=weatherData.main.temp;
-                const weatherDiscription=weatherData.weather[0].description
-                const icon =weatherData.weather[0].icon
-                // console.log(temp)
-                res.send(`
+        respos.on("data", (data) => {
+            const weatherData = JSON.parse(data);
+            //  console.log(weatherData);
+            const temp = weatherData.main.temp;
+            const weatherDiscription = weatherData.weather[0].description
+            const icon = weatherData.weather[0].icon
+            // console.log(temp)
+            res.send(`
                 <h1> 
                 temp at ciity ${query} =${temp}
                 and the climate is =${weatherDiscription};
                 </h1>
                 <img src='http://openweathermap.org/img/wn/${icon}@2x.png'>
                 `)
-                // <img src="" alt="">
+            // <img src="" alt="">
         })
     })
-    
+
 
 })
 
 
-app.listen(5000,()=>{
-console.log('The serving running at port 5000');
+app.listen(5000, () => {
+    console.log('The serving running at port 5000');
 })
